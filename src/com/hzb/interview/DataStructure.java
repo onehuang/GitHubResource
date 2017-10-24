6935c3d4959ee5c9e70f670ac05d2996c492d8df
zibeen.blog <1576681705@qq.com>
2017-10-24 22:56:30 +0800

    新增java线程的练习javaThread.java文件

--- src/com/hzb/interview/DataStructure.java
+++ src/com/hzb/interview/DataStructure.java
@@ -1,4 +1,95 @@
 package com.hzb.interview;
 
+import java.util.Stack;
+
 public class DataStructure {
+    public static void main(String[] args){
+        new binaryTreeVisit().run();
+    }
+}
+
+class binaryTreeVisit{
+    public void run(){
+        BinaryTree node = new BinaryTree("A");
+        BinaryTree root = node;
+
+        BinaryTree node1 = new BinaryTree("B");
+        node.setLeft(node1);
+
+        BinaryTree node2 = new BinaryTree("C");
+        node.setRight(node2);
+
+        BinaryTree node3 = new BinaryTree("D");
+        node1.setRight(node3);
+
+        BinaryTree node4 = new BinaryTree("E");
+        node3.setRight(node4);
+        BinaryTree node5 = new BinaryTree("F");
+        node3.setLeft(node5);
+
+        postVisitTree(root);
+    }
+
+    class BinaryTree{
+
+        private String name;
+        private BinaryTree left;
+        private BinaryTree right;
+
+        public BinaryTree(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        public BinaryTree getLeft() {
+            return left;
+        }
+
+        public void setLeft(BinaryTree left) {
+            this.left = left;
+        }
+
+        public BinaryTree getRight() {
+            return right;
+        }
+
+        public void setRight(BinaryTree right) {
+            this.right = right;
+        }
+        public void visit(){
+            System.out.print(name + " ");
+        }
+    }
+
+    //非递归后序遍历
+    public void postVisitTree(BinaryTree root){
+        Stack<BinaryTree> stack = new Stack();
+        for(BinaryTree  node = root; !stack.empty() || node != null; ){
+            //将左节点放入栈中
+            while(node != null){
+                stack.push(node);
+                node = node.getLeft();
+            }
+            //弹出节点
+            while(!stack.empty() && node == stack.peek().getRight()){
+                node = stack.pop();
+                node.visit();
+            }
+
+            //
+            if(stack.empty()){
+                return;
+            }else{
+                node = stack.peek().getRight();
+            }
+        }
+
+    }
+
 }
--- src/com/hzb/javaThread.java
+++ src/com/hzb/javaThread.java
@@ -2,6 +2,7 @@ package com.hzb;
 
 import java.util.Random;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by zibeen on 2017/10/6.
@@ -31,6 +32,10 @@ public class javaThread  {
 //        ThreadCountDownLatch.run();
 //        ThreadCyclicBarrier.run();
 //        ThreadSemaphore.run();
+//        SellTicket.run();
+//        ThreadDataExchange.run();
+//        ThreadProductAndConsumer.run();
+        ThreadVolatile.run();
     }
 
 }
@@ -157,21 +162,21 @@ class ThreadCyclicBarrier{
  * 8个工人操作5台机器，一台机器同时只能被一个工人使用。
  */
 class ThreadSemaphore{
-    public static final int N = 8;  //工人数量；
-    public static final int M = 5;  //机器的数量；
+    public static final int N = 4;  //工人数量；
+    public static final int M = 7;  //机器的数量；
     public static void run(){
-        final Semaphore ah = new Semaphore(5);
+        final Semaphore ah = new Semaphore(M);
 
         for(int i = 0; i < N; i++){
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        ah.acquire();
+                        ah.acquire(2);
                         System.out.println(Thread.currentThread().getName()+"正在使用机器...");
                         Thread.sleep(1000);
                         System.out.println(Thread.currentThread().getName()+"使用机器完成");
-                        ah.release();
+                        ah.release(2);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -180,7 +185,7 @@ class ThreadSemaphore{
             },  i +"号工人").start();
         }
         while (ah.availablePermits() < M){
-            System.out.println("当前正在使用的机器数量: " + (M - ah.availablePermits()));
+            System.out.println("当前剩余机器数量: " + ah.availablePermits());
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
@@ -189,3 +194,223 @@ class ThreadSemaphore{
         }
     }
 }
+
+class SellTicket {
+
+    static class Ticket implements Runnable{
+
+        private int ticket = 10;
+        public void run() {
+
+            synchronized(Ticket.class){
+                while(ticket>0){
+                    ticket--;
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println( Thread.currentThread().getName() +"当前票数为："+ticket);
+                }
+            }
+
+        }
+
+    }
+
+    public static void run() {
+        Ticket t = new Ticket();
+        new Thread(t, "线程1: ").start();
+        new Thread(t, "线程2: ").start();
+    }
+
+}
+
+/**
+ * 线程间数据交互
+ * 4个线程，2个线程对i加1,2个线程对i减1
+ * 不同的操作要在不同线程中进行
+ */
+class ThreadDataExchange{
+    private static final int num = 4;
+    private static final Data dt = new Data();
+
+    public static void run(){
+        for(int i=0; i < num / 2; i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dt.add();
+                }
+            }, "线程：" + i).start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dt.minus();
+                }
+            }, "线程：" +  ( i +num / 2) ).start();
+        }
+    }
+    static class Data{
+        private int i = 0;
+
+        /**
+         * 对i增加1
+         */
+        public synchronized void add(){
+            i++;
+            System.out.println(Thread.currentThread().getName()+" 对i增加 1： " + i);
+        }
+
+        /**
+         * 对i减1
+         */
+        public synchronized void minus(){
+            i--;
+            System.out.println(Thread.currentThread().getName()+" 对i减少 1: " + i);
+        }
+    }
+
+}
+
+/**
+ * 生产和消费者
+ */
+class ThreadProductAndConsumer{
+
+    private static final int ProductorNum = 10;
+    private final static int ConsumerNum = 10;
+    public static void run(){
+        Data data = new Data();
+        for(int i = 0; i < ProductorNum; i ++){
+            new Thread( new producter(data), "生产者 " + i + " 号").start();
+        }
+
+        for(int i = 0; i < ConsumerNum; i ++){
+            new Thread( new consumer(data), "消费者 " + i + " 号").start();
+        }
+    }
+
+    static class Data{
+
+        private final int MixNum = 100;
+        public int Num = 0;
+
+        Data(){
+            System.out.println("初始化了仓库...");
+        }
+
+        public synchronized void product(){
+            Num ++;
+            System.out.println(Thread.currentThread().getName()+" 生产者生产了一个产品, 总共："+ Num);
+            notify();
+        }
+
+        public synchronized void consume(){
+            Num --;
+            System.out.println(Thread.currentThread().getName()+" 消费者消费了一个产品, 总共："+ Num);
+            notify();
+        }
+
+        public synchronized boolean isFull(){
+            if (Num >= MixNum){
+                try {
+                    System.out.println(Thread.currentThread().getName()+" 仓库已满，等待消费者消费 ");
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return Num >= MixNum;
+        }
+
+        public synchronized boolean isEmpty(){
+
+            if (Num <= 0){
+                try {
+                    System.out.println(Thread.currentThread().getName()+" 仓库已空，等待生产者生产 ");
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return Num <= 0;
+        }
+    }
+
+    /**
+     * 生产者
+     */
+    static class producter implements Runnable{
+        private Data data;
+        public producter(Data data){
+            this.data = data;
+        }
+        @Override
+        public void run() {
+            while (true){
+                if( data.isFull() ){
+                    continue;
+                }
+                data.product();
+
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 消费者
+     */
+    static class consumer implements Runnable{
+        private Data data;
+        public consumer(Data data){
+            this.data = data;
+        }
+        @Override
+        public void run() {
+            while (true){
+                if(data.isEmpty()){
+                    continue;
+                }
+                data.consume();
+
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
+
+class ThreadVolatile{
+    public static volatile int n =0;
+
+
+    public static void run(){
+         AtomicInteger ai = new AtomicInteger(0);
+        for(int i = 0; i < 100; i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    n ++;
+                    ai.getAndIncrement();
+//                    System.out.println(Thread.currentThread().getName()+" volatile n = " + n);
+                    System.out.println(Thread.currentThread().getName()+" ai = " + ai.get());
+                }
+            }, "线程:" + i).start();
+        }
+
+
+
+    }
+
+}
+
